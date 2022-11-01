73e88f9a2233263af06c7b8aa17e6c1d48371b73
cxk_git <1720921998@qq.com>
2022-11-01 12:48:16 +0800

    change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,19 @@
 <template>
-    <HomeHeader></HomeHeader>
+    <div>
+        <HomeHeader></HomeHeader>
+        <HomeSwiper></HomeSwiper>
+
+    </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
 export default {
     name: 'Home',
     components: {
-        HomeHeader
+        HomeHeader,
+        HomeSwiper
     }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl">
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data() {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true,
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl: 'https://source.qunarzz.com/site/images/wns/20221031_qunar_dujia_homepage_a750x192_v6.jpg'
+            }, {
+                id: '0002',
+                imgUrl: 'https://source.qunarzz.com/site/images/wns/20221020_qunar_dujia_homepage_a750x192_v2.jpg'
+            }]
+        }
+    }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+    .wrapper
+        overflow:hidden
+        width: 100%
+        height: 0
+        padding-bottom: 25.8%
+        // padding-bottom: 31.25%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
