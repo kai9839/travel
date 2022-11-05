e1121287aa8b332b0358f3c11f80c9f130483335
cxk_git <1720921998@qq.com>
2022-11-06 00:31:19 +0800

    add ZiMuLan

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -3,6 +3,7 @@
         <CityHeader></CityHeader>
         <CitySearch></CitySearch>
         <CitySList></CitySList>
+        <CityAlphabet></CityAlphabet>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CitySList from './components/List'
+import CityAlphabet from './components/Alphabet.vue'
 export default {
     name: 'City',
     components: {
         CityHeader,
         CitySearch,
         CitySList,
+        CityAlphabet
     }
 }
 </script>
