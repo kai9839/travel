da494af94ca28d139dd8a6e6eeed2d116bcc5864
cxk_git <1720921998@qq.com>
2022-11-06 23:21:09 +0800

    add city-search-logic

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <city-header></city-header>
-        <city-search></city-search>
+        <city-search :cities="cities"></city-search>
         <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
         <city-alphabet :cities="cities" @change="handleLetterClick"></city-alphabet>
     </div>
@@ -33,7 +33,6 @@ export default {
             axios.get('api/city.json').then(this.handleGetCityInfoSucc)
         },
         handleGetCityInfoSucc(res) {
-            console.log(res);
             res = res.data;
             if (res.ret && res.data) {
                 const data = res.data;
