4de5f515e84bfa4b7ef6a56d69cb02fbd44e5549
cxk_git <1720921998@qq.com>
2022-11-06 17:53:17 +0800

    add city-ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,28 +1,56 @@
 <template>
     <div>
-        <CityHeader></CityHeader>
-        <CitySearch></CitySearch>
-        <CitySList></CitySList>
-        <CityAlphabet></CityAlphabet>
+        <city-header></city-header>
+        <city-search></city-search>
+        <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+        <city-alphabet :cities="cities" @change="handleLetterClick"></city-alphabet>
     </div>
 </template>
 
 <script>
-import CityHeader from './components/Header'
-import CitySearch from './components/Search'
-import CitySList from './components/List'
+import axios from 'axios'
+import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
+import CityList from './components/List.vue'
 import CityAlphabet from './components/Alphabet.vue'
 export default {
     name: 'City',
     components: {
         CityHeader,
         CitySearch,
-        CitySList,
+        CityList,
         CityAlphabet
+    },
+    data() {
+        return {
+            cities: {},
+            hotCities: [],
+            letter: ''
+        }
+    },
+    methods: {
+        getCityInfo() {
+            axios.get('api/city.json').then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc(res) {
+            console.log(res);
+            res = res.data;
+            if (res.ret && res.data) {
+                const data = res.data;
+                this.cities = data.cities;
+                this.hotCities = data.hotCities;
+            }
+        },
+        handleLetterClick(letter) {
+            this.letter = letter;
+        }
+    },
+    mounted() {
+        this.getCityInfo();
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+	
 </style>
